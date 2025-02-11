edd949c71a11f234e22b57438f484ff456ada4a7
javiern-778 <jcruzespana2@gmail.com>
2025-02-11 08:24:38 -0600

    Agregando segundo ejercicio

---
+++ ejercicio2.py
@@ -0,0 +1,14 @@
+#declarar variable 
+nombre = "Javier"
+edad = 24
+altura = 1.70
+
+#Imprimir informacion 
+
+print ("Nombre", nombre)
+print ("Edad", edad)
+print("altura", altura)
+
+#Operacion simple
+edad_en_5_anos= edad + 5
+print("En 5 años, tendre", edad_en_5_anos, "años.") 
